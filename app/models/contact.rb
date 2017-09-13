2dd0d8d004073e104ad5d62e41e35f1dc8a157d4
Ashraf Nazar <ashraf.caspian@gmail.com>
2017-09-13 20:28:29 +0100

    init

---
+++ app/models/contact.rb
@@ -0,0 +1,16 @@
+class Contact < MailForm::Base
+	attribute :name, validate: true
+	attribute :email
+	validates_format_of :email, with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+	attribute :subject, validate: true
+	attribute :message,  validate: true
+	attribute :nickname, captcha: true
+
+	def headers
+		{
+			subject: 'Contact Form',
+			to: 'ashraf.caspian@gmail.com',
+			from: %("#{name}" <#{email}>)
+		}
+	end
+end
