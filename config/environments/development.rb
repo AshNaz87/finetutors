55f70e6dbaf8f97ac55dee2112aee9a0e7cfb617
Ashraf Nazar <ashraf.caspian@gmail.com>
2017-09-13 20:40:59 +0100

    changed database to postgresql

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,18 +29,6 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.delivery_method  = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: 'smtp@gmail.com',
-    port: 587,
-    domain: 'finetutors.co.uk',
-    username: 'username@gmail.com',
-    password: 'password',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
-
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
